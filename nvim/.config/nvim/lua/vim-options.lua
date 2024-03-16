f1f71e02a53fab22ea80a1831bb7fcbf66725ef2
ema0898 <aguisnchez@gmail.com>
2024-03-16 12:31:25 -0600

    Add nvim configuration

---
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,17 @@
+-- Leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Misc configuration
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+
+vim.api.nvim_set_option("clipboard","unnamed")
