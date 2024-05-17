82492094c57c5c1c17d6124cbee1e4656a1522d4
ema0898 <aguisnchez@gmail.com>
2024-05-16 22:39:35 -0600

    Update completions keybindings

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -27,11 +27,10 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }), -- previous suggestion
+          ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }), -- next suggestion
+          ['<C-e>'] = cmp.mapping.abort(),
+					["<Tab>"] = cmp.mapping.confirm({ select = true }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
